2d02edf2579fdcbea84a07c320891670611baf50
carlitos321 <ca.cabrera321@gmail.com>
2018-02-05 20:57:46 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -2,3 +2,19 @@
 function paragraphSelector(){
   return $('p')
 }
+
+function lastImageSelector(){
+  return $('img:last')
+}
+
+function ninjaBabySelector(){
+
+}
+
+function divSelector(){
+
+}
+
+function firstListItemSelector(){
+
+}
